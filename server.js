25427a013c846048ceb713cb75a4e13da479539f
Rob <rlovelett@gmail.com>
2017-03-22 16:40:52 -0500

    Set up mongoose & heroku connectivity

---
+++ server.js
@@ -0,0 +1,45 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Required Models
+var config = require('./config');
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect(config.mongodb_uri);
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function (error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function () {
+  console.log("Mongoose connection successful.");
+});
+
+// Routes
+// ======
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log("App running on port 3000!");
+});
