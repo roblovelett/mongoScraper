6b576f6070aa943843df722569975e5fda327e1b
rob <rob>
2017-03-22 21:10:21 -0500

    trying to get heroku db to work

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/scraper" || config.mongodb_uri);
+mongoose.connect(config.mongodb_uri || 'mongodb://localhost/scraper');
 var db = mongoose.connection;
 
 // Show any mongoose errors
